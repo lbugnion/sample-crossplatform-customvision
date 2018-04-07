5a4eabdab5096a49b772a5e3399be54a5440f324
lbugnion <lbugnion@microsoft.com>
2018-04-07 10:01:44 +0200

    Removed keys (no big deal, just want to avoid issue with deleted custom vision projects).

--- CustomVisionClient/CustomVisionClient/CustomVisionClient/App.xaml.cs
+++ CustomVisionClient/CustomVisionClient/CustomVisionClient/App.xaml.cs
@@ -4,8 +4,8 @@ namespace CustomVisionClient
 {
     public partial class App : Application
     {
-        public const string PredictionKey = "3a23be17748a44cdbbedf9ad2ff36c93";
-        public const string ProjectId = "02447956-3451-4be8-bf32-bdc652c94b83";
+        public const string PredictionKey = "PREDICTIONKEY";
+        public const string ProjectId = "PROJECTID";
 
         public App()
         {
